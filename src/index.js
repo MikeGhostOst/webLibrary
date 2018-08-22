c0d2bf01a32a2aa348d3296f9a082aab7dfaf685
MikeGhost <mihail.ostras@rambler.ru>
2018-08-22 14:40:22 +0300

    Fixed webpack configuration. "Dist" folder deleted. Readme updated.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import styles from './style.css';
 import Header from './Header.js';
 import SearchForm from './SearchForm.js';
+import indexHTML from './index.html';
+import booksJSON from './books.json';
+import serverJS from './server.js';
+
 
 class App extends React.Component {
     constructor(props) {
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
       },
     module: {
         rules: [
+          { test: /\.html$/, loader: "file-loader", options: { name: '[name].[ext]' } },
+          { test: /\.json$/, options: { name: '[name].[ext]' }, loader: "file-loader", type: "javascript/auto" },
+          { test: /server.js/, loader: "file-loader", options: { name: '[name].[ext]' } },
           { test: /\.js$/, loader: "babel-loader" },
           { test: /\.css$/, loader: "style-loader!css-loader" },
         ]
